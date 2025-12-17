8f62e12aed5d07ece406a6c27350b466ba449361
Lehi Muñoz <lehi.munoz@gmail.com>
2025-12-17 07:34:12 -0600

    Add files via upload

---
+++ SoulGUI/include/Soul_GUI.h
@@ -0,0 +1,16 @@
+/********************************************************************
+LC			09/08/2016
+Created:	2016/09/08
+Filename:	Soul_GUI.h
+Author:		Rubén Rodríguez
+
+Purpose:	Cabecera que incluye las librerias linkeadas asi como los
+archivos propios de la clase
+*********************************************************************/
+#pragma once
+
+/************************************************************************/
+/* Inclucion de cabeceras que incluye la libreria                       */
+/************************************************************************/
+#include "soFastDelegate.h"
+#include "soFastDelegateBind.h"
